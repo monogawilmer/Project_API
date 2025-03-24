cb6da51b631295d65af8b6989e16a5a6a2735675
Hart <monogawilmer@gmail.com>
2025-03-24 17:40:33 -0500

    Se agrega Controlador de Rol y TipoDocumento y se instala JWT

---
+++ API/Controllers/TipoDocumentoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project_API.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoDocumentoController : ControllerBase
+    {
+    }
+}
